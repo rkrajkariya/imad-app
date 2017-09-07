ce119636930ee2e19a85f54378ce847c61657159
rkrajkariya <31384086+rkrajkariya@users.noreply.github.com>
2017-09-07 10:50:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
 
-var element = document.getElementById('img');
+var element = document.getElementById('madi');
 img.onclick = function() {
   umg.style.marginLeft = '100px';
 };

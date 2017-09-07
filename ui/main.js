64563e491e9a6e27a4d94940ecb4464854ee3624
rkrajkariya <31384086+rkrajkariya@users.noreply.github.com>
2017-09-07 10:49:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
+
+var element = document.getElementById('img');
+img.onclick = function() {
+  umg.style.marginLeft = '100px';
+};
